072c8df5e6d8257a259dbad1c1308d1775904ba2
abhishek14299 <abhishek14299@gmail.com>
2025-04-05 19:23:38 +0530

    Binary Search Algorithm

---
+++ BinarySearch/src/binarySearch/BinarySearch.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package binarySearch;
+
+/**
+ * 
+ */
+public class BinarySearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
